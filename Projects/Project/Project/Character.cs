16931e5f5ece9170c4576b44f366bd26eaa78f9b
Henna <qureshi.henna@gmail.com>
2022-05-25 17:24:49 +0200

    Adding dist subtree

---
+++ Projects/Project/Project/Character.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Project
+{
+    public class Character
+    {
+        public Character()
+        {
+        }
+    }
+}
---
+++ Projects/Project/Project/Monster.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Project
+{
+    public class Monster
+    {
+        public Monster()
+        {
+        }
+    }
+}
